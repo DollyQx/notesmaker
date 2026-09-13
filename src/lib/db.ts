71ebc3722bd4e1e0e5dfa71db7b658b8b09d7d37
DollyQx <dollykumarilinux@gmail.com>
2026-09-13 17:03:23 +0530

    fix(vercel): trigger prisma generate during build and fallback db url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// Fallback dummy connection string for build-time safety when DATABASE_URL is not set
+// Fallback connection string for Vercel static build evaluation when DATABASE_URL is not set
 const fallbackUrl = 'postgresql://postgres:postgres@localhost:5432/notesmaker';
 
 export const prisma =
